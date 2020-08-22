b7fb55d3a36557b836f3254bcf068a8c90362041
Benardi <benardinunes@gmail.com>
2020-08-22 18:43:04 +0000

    Add priv key verification to Role requests in Node

--- apps/node/src/app/main/core/exceptions.py
+++ apps/node/src/app/main/core/exceptions.py
@@ -6,6 +6,12 @@ class PyGridError(Exception):
         super().__init__(message)
 
 
+class AuthorizationError(PyGridError):
+    def __init__(self):
+        message = "User is not authorized for this operation!"
+        super().__init__(message)
+
+
 class WorkerNotFoundError(PyGridError):
     def __init__(self):
         message = "Worker ID not found!"
@@ -15,6 +21,12 @@ class WorkerNotFoundError(PyGridError):
 class RoleNotFoundError(PyGridError):
     def __init__(self):
         message = "Role ID not found!"
+        super().__init__(message)
+
+
+class UserNotFoundError(PyGridError):
+    def __init__(self):
+        message = "User ID not found!"
         super().__init__(message)
 
 
@@ -87,6 +99,12 @@ class CheckPointNotFound(PyGridError):
 class InvalidRequestKeyError(PyGridError):
     def __init__(self):
         message = "Invalid request key!"
+        super().__init__(message)
+
+
+class MissingRequestKeyError(PyGridError):
+    def __init__(self):
+        message = "Missing request key!"
         super().__init__(message)
 
 
